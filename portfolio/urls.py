8194be0def9e388f3e2bfaec19c1c585f2e86aab
nancyruya <nancyruya@gmail.com>
2020-02-26 21:54:25 -0500

    presented data to website

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 import cmccDemos.views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url('', cmccDemos.views.homepage, name ='home'),
-]
+] 
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
