b05dcf061c671951bc01ee74c6a9afb6c7bae819
nancyruya <nancyruya@gmail.com>
2020-03-11 20:57:25 -0400

    added a new model, made migrations, I'm no longer doing what I intended to do.because the picture is not showing

--- cmccDemos/models.py
+++ cmccDemos/models.py
@@ -3,9 +3,12 @@ from django.db import models
 # Create your models here.
 class CmccDemo(models.Model):
 	#images
-	image = models.ImageField(upload_to = 'images/')
+	image = models.FileField(upload_to = 'images/')
 	#summary
 	summary = models.CharField(max_length=200)
+	#description
+	new_field = models.CharField(max_length=140, default='SOME STRING')
+
 
 	def __str__(self):
 		return self.summary
