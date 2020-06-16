986a8dcc6cd766eebbadca622ff18d253186f3cb
nancyruya <nancyruya@gmail.com>
2020-06-15 20:40:40 -0400

    everything is bugging with me

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -74,10 +74,10 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
+DATABASES = { 
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'portfoliodb',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
         'USER':'postgres',
         'PASSWORD':'1234',
         'HOST':'localhost',
